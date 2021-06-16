2ff1fba1aaf975ecacb63afb95a4c90cc77a0330
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-06-16 22:30:18 +0900

    Update deps

--- vendor/knative.dev/networking/pkg/apis/networking/v1alpha1/ingress_defaults.go
+++ vendor/knative.dev/networking/pkg/apis/networking/v1alpha1/ingress_defaults.go
@@ -41,7 +41,11 @@ func (is *IngressSpec) SetDefaults(ctx context.Context) {
 }
 
 // SetDefaults populates default values in IngressTLS
-func (t *IngressTLS) SetDefaults(ctx context.Context) {}
+func (t *IngressTLS) SetDefaults(ctx context.Context) {
+	// Deprecated, do not use.
+	t.DeprecatedServerCertificate = ""
+	t.DeprecatedPrivateKey = ""
+}
 
 // SetDefaults populates default values in IngressRule
 func (r *IngressRule) SetDefaults(ctx context.Context) {
--- vendor/knative.dev/networking/test/conformance/ingress/httpoption.go
+++ vendor/knative.dev/networking/test/conformance/ingress/httpoption.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 The Knative Authors
+Copyright 2021 The Knative Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ package ingress
 
 import (
 	"context"
+	"net/http"
 	"testing"
 
 	"k8s.io/apimachinery/pkg/util/intstr"
@@ -26,11 +27,39 @@ import (
 	"knative.dev/networking/test"
 )
 
-// TestIngressTLS verifies that the Ingress properly handles the TLS field.
-func TestIngressTLS(t *testing.T) {
+// TestHTTPOption verifies that the Ingress properly handles HTTPOption field.
+func TestHTTPOption(t *testing.T) {
 	t.Parallel()
 	ctx, clients := context.Background(), test.Setup(t)
 
+	tests := []struct {
+		httpOption v1alpha1.HTTPOption
+		code       int
+	}{{
+		httpOption: v1alpha1.HTTPOptionEnabled,
+		code:       http.StatusOK,
+	}, {
+		httpOption: v1alpha1.HTTPOptionRedirected,
+		code:       http.StatusMovedPermanently,
+	}}
+
+	hostCode := make(map[string]int, len(tests))
+	var client *http.Client
+	// Create multiple ingress with different HTTP option at the same time.
+	// This makes sure that each Ingress's HTTP option does not effect on globally.
+	for _, test := range tests {
+		var host string
+		host, client = create(ctx, t, clients, test.httpOption)
+		hostCode[host] = test.code
+	}
+
+	// Request to each Ingress.
+	for host, code := range hostCode {
+		checkHTTPOption(ctx, t, client, host, code)
+	}
+}
+
+func create(ctx context.Context, t *testing.T, clients *test.Clients, httpOption v1alpha1.HTTPOption) (string, *http.Client) {
 	name, port, _ := CreateRuntimeService(ctx, t, clients, networking.ServicePortNameHTTP1)
 
 	hosts := []string{name + ".example.com"}
@@ -38,7 +67,7 @@ func TestIngressTLS(t *testing.T) {
 	secretName, _ := CreateTLSSecret(ctx, t, clients, hosts)
 
 	_, client, _ := CreateIngressReady(ctx, t, clients, v1alpha1.IngressSpec{
-		HTTPOption: v1alpha1.HTTPOptionEnabled,
+		HTTPOption: httpOption,
 		Rules: []v1alpha1.IngressRule{{
 			Hosts:      hosts,
 			Visibility: v1alpha1.IngressVisibilityExternalIP,
@@ -60,12 +89,26 @@ func TestIngressTLS(t *testing.T) {
 			SecretNamespace: test.ServingNamespace,
 		}},
 	})
-
-	// Check without TLS.
-	RuntimeRequest(ctx, t, client, "http://"+name+".example.com")
-
-	// Check with TLS.
-	RuntimeRequest(ctx, t, client, "https://"+name+".example.com")
+	return hosts[0], client
 }
 
-// TODO(mattmoor): Consider adding variants where we have multiple hosts with distinct certificates.
+func checkHTTPOption(ctx context.Context, t *testing.T, client *http.Client, hostname string, code int) {
+	// Check with TLS.
+	RuntimeRequest(ctx, t, client, "https://"+hostname)
+
+	// Check without TLS.
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		// Do not follow redirect.
+		return http.ErrUseLastResponse
+	}
+	resp, err := client.Get("http://" + hostname)
+	if err != nil {
+		t.Fatal("Error making GET request:", err)
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != code {
+		t.Errorf("Unexpected status code: %d, wanted %v", resp.StatusCode, code)
+		DumpResponse(ctx, t, resp)
+	}
+}
